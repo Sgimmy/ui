33ce3e98d36623dd2df4fa3d3e8d901d2109949f
Simone Gizzi <simonegizzi85@gmail.com>
2020-09-10 08:50:27 +0200

    add filters component

--- src/components/list/layout/filters/index.tsx
+++ src/components/list/layout/filters/index.tsx
@@ -4,7 +4,14 @@ import styled from 'styled-components';
 const Filters = () => {
   return (
     <Container>
-
+      <Select>
+        <option value="all">Tutte le categorie</option>
+        <option value="react">React</option>
+        <option value="redux">Redux</option>
+        <option value="angular">Angular</option>
+        <option value="ngrx">NgRx</option>
+        <option value="rxjs">RxJS</option>
+      </Select>
     </Container>
   );
 };
@@ -12,5 +19,13 @@ const Filters = () => {
 export default Filters;
 
 const Container = styled.div`
+  margin-top: 20px;
+  border-radius: 8px;
+  border: 1px solid ${props => props.theme.colors.borders};
+  overflow: hidden;
+`;
 
+const Select = styled.select`
+  padding: 15px;
+  width: 100%;
 `;
